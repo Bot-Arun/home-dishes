2570cd7457df5021894a93bf34701008ff94b7bc
arun <arunmugunthan07@gmail.com>
2022-07-14 10:17:11 +0530

    added simple login page

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, NativeModules, TextInput, Pressable, TouchableOpacity, TouchableNativeFeedback, TouchableOpacityComponent, Touchable, Platform } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import NumericInput from 'react-native-numeric-input';
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+const {StatusBarManager} = NativeModules;
+var statusbar_from_os = 0 ;
+if(Platform.OS != 'web') {
+  var statusbar_from_os = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
+} 
+const STATUSBAR_HEIGHT =  statusbar_from_os;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <View className={"flex  h-full mt-10 "} style={{marginTop:STATUSBAR_HEIGHT}} >  
+        <View className="mt-20 p-10">
+          <Text className="text-center font-semibold"  style={{fontSize:35}}> Login </Text>
+        </View>
+        <View className="p-10 ">
+          <Text className="" style={{fontSize:20}}>
+            User Name
+          </Text>
+          <TextInput className="border mt-5 p-3" placeholder='username' /> 
+          <Text className="mt-10" style={{fontSize:20}}>
+            Password
+          </Text>
+          <TextInput className="border mt-5  p-3" placeholder='password' secureTextEntry={true} style={styles.default}  />
+          <Text className="mt-1 text-right" style={{fontSize:15}}>
+            Forget password ? 
+          </Text>
+          <TouchableOpacity className="mt-16">
+            <Text className="text-center p-3 text-white  bg-blue-700" style={{fontSize:20}}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          
+        </View>
+        
+      </View>
+    </TailwindProvider>
   );
 }
 
