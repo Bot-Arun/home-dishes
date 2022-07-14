821bfa8e659eb59515736d2c0c1b58dcfd50c89f
arun <arunmugunthan07@gmail.com>
2022-07-14 10:18:30 +0530

    added tailwind.config file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [],
+    content: ["./*.{js,jsx,ts,tsx}","./components/*.{js,jsx,ts,tsx}" , "./src/**/*.{js,jsx,ts,tsx}"],
+     theme: {
+       extend: {},
+     },
+      plugins: [],
+}
