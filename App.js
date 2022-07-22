3946f95fa6ce4e24a6c0aa24dc4467ed3f29b473
arun <arunmugunthan07@gmail.com>
2022-07-23 02:36:44 +0530

    UI changes & FlatList to ScrollView , to home

--- App.js
+++ App.js
@@ -1,33 +1,93 @@
-import  {createStackNavigator} from '@react-navigation/stack'
-import  {NavigationContainer} from '@react-navigation/native'
-import { Ionicons ,MaterialIcons,FontAwesome} from '@expo/vector-icons';
-import 'react-native-gesture-handler';
-import Home from './components/Home'
-import History from './components/History'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Login from './components/Login'
-import Profile from './components/Profile'
-const Pages = ({navigation}) => {
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons, MaterialIcons, FontAwesome } from "@expo/vector-icons";
+import "react-native-gesture-handler";
+import Home from "./components/Home";
+import History from "./components/History";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import { View,Text } from "react-native";
+const Pages = ({ navigation }) => {
   const BottomNavigator = createMaterialBottomTabNavigator();
   return (
-    <BottomNavigator.Navigator screenOptions={{gestureEnabled:false, presentation:'modal'}}   initialRouteName='Login' >
-        <BottomNavigator.Screen  name="Home" options={{tabBarIcon:({color})=>(<Ionicons name="home" size={24} color={color} />)}} component={Home} />
-        <BottomNavigator.Screen name="History" options={{tabBarIcon:({color})=>(<MaterialIcons name="history" size={27} color={color} />)}} component={History} />
+    <BottomNavigator.Navigator
+      screenOptions={{ gestureEnabled: false, presentation: "modal" }}
+      initialRouteName="Login"
+    >
+      <BottomNavigator.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+        component={Home}
+      />
+      <BottomNavigator.Screen
+        name="History"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="history" size={27} color={color} />
+          ),
+        }}
+        component={History}
+      />
     </BottomNavigator.Navigator>
-    )
-}
+  );
+};
 
 export default function App() {
-  const BottomNavigator = createMaterialBottomTabNavigator();
+  const BottomNavigator = createBottomTabNavigator ();
   return (
-  <NavigationContainer>
-    <BottomNavigator.Navigator screenOptions={{gestureEnabled:false, presentation:'modal'}}   initialRouteName='Login' >
-        <BottomNavigator.Screen  name="Home" options={{tabBarIcon:({color})=>(<Ionicons name="home" size={24} color={color} />)}} component={Home} />
-        <BottomNavigator.Screen name="History" options={{tabBarIcon:({color})=>(<MaterialIcons name="history" size={27} color={color} />)}} component={History} />
-        <BottomNavigator.Screen name="Profile" options={{tabBarIcon:({color})=>(<FontAwesome name="user" size={24} color={color} />)}} component={Profile} />
-    </BottomNavigator.Navigator>
-  </NavigationContainer>
-    )
+    <NavigationContainer>
+      <BottomNavigator.Navigator
+        barStyle={{
+          height:64,
+          backgroundColor: "white",
+          borderColor: "black",
+        }}
+        screenOptions={{ gestureEnabled: false, presentation: "modal",headerStyle: {
+          height: 0, // Specify the height of your custom header
+        } ,
+        tabBarActiveTintColor:'red',
+        tabBarItemStyle:{height:60,padding:5},
+        tabBarStyle:{height:60,backgroundColor:'white'}
+      }}
+        initialRouteName="Login"
+      >
+        <BottomNavigator.Screen
+          name="Home"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home" size={30} color={color} />
+            ),
+            tabBarColor: "red",
+          }}
+          component={Home}
+        />
+        <BottomNavigator.Screen
+          name="History"
+          options={{
+            tabBarColor:'pink',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="history" size={36} color={color} />
+            ),
+          }}
+          component={History}
+        />
+        <BottomNavigator.Screen 
+          name="Profile"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" size={30} color={color} />
+            ),
+            tabBArButton:<Pressable className="bg-slate-500"><Text>dsklfjsdf</Text></Pressable>
+          }}
+          component={Profile}
+        />
+      </BottomNavigator.Navigator>
+    </NavigationContainer>
+  );
 }
-
-
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,68 +1,208 @@
-import { useEffect, useState } from 'react';
-import {Alert, BackHandler, SafeAreaView, Text,TouchableOpacity, View} from 'react-native'
-import {MaterialIcons,Ionicons} from '@expo/vector-icons'
-import { TailwindProvider } from 'tailwindcss-react-native';
-import Screen from './Screen';
-import { ScrollView } from 'react-native-gesture-handler';
+import {
+  ActivityIndicator,
+  Text,
+  Image,
+  ImageBackground,
+  VirtualizedList,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  ListViewBase,
+} from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import Screen from "./Screen";
+import RBSheet from "react-native-raw-bottom-sheet";
+import { useRef } from "react";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
-const Home = ({navigation}) => {
-    useEffect(() => {
-        const backAction = () => {
-          Alert.alert("Hold on!", "Are you sure you want to quit the App?", [
-            {
-              text: "Cancel",
-              onPress: () => null,
-              style: "cancel"
-            },
-            { text: "YES", onPress: () => BackHandler.exitApp() }
-          ]);
-          return true;
-        };
-    
-        const backHandler = BackHandler.addEventListener(
-          "hardwareBackPress",
-          backAction
-        );
-    
-        return () => backHandler.remove();
-    }, []);
-    const [freeq,setFreeq] = useState(0);
-    const [items,setItems] =useState([])
-    const setItem = (id,val)=> {
-       items[id].item =val
-       setItems([...items]);
-    //    console.log(items)
-    }
-    const setCount = (id,val)=> {
-        items[id].value =val
-        setItems([...items]);
-        // console.log([...items])
-    }
-    return (<Screen>
+const Separotor = () => {
+  return <View className="mx-4  bg-gray-300" style={{ paddingTop: 1 }} />;
+};
+const Item = ({ title, price, desc, img }) => {
+  const model = useRef();
+  return (
+    <View className="w-full g flex rounded-3xl flex-row justify-between py-5 bg-white my-2  "style={{elevation:10,shadowColor:'white'}} >
+      <View className="header pl-9  p-5 flex-1">
+        <Text className="text-xl text-left pt-5 text-red-600 font-bold">{title} </Text>
+        <Text className="py-3 text-left text-lg font-semibold">₹{price}</Text>
+        <View className="flex-row">
+          <Text
+            className=" text-md-center text-gray-500 text-md"
+            numberOfLines={10}
+            style={{ flexWrap: "wrap", flex: 1 }}
+          >
+            {" "}
+            {desc}
+          </Text>
+        </View>
+      </View>
       
-    <TailwindProvider>
-        <View className="p-10 flex flex-col py-20   justify-center ">
-            <Text className="text-center text-2xl">Chose your Choice</Text>
-        <ScrollView className=" ">
-                
-        <View className=" flex flex-row justify-around">
+      <View className="ml-auto  pt-5 pr-5">
+        <Pressable onPress={() => model.current.open()}>
+        <View className="bg-gray-100  rounded-2xl " style={{ elevation: 5 }}>
+          <Image
+            style={{
+              resizeMode: "contain",
+              shadowColor: "black",
+              width: 150,
+              height: 150,
+            }}
+            source={img}
+            />
+        </View>
+            </Pressable>
+        <TouchableNativeFeedback >
+          <Text
+            style={{ elevation: 4 }}
+            className="text-white font-bold py-2 px-9 text-lg -translate-y-2 self-center rounded-xl bg-red-500 text-center "
+          >
+            ADD
+          </Text>
+        </TouchableNativeFeedback>
+      </View>
+      <RBSheet
+        ref={model}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        height={400}
+        customStyles={{
+          wrapper: {
+            backgroundColor: "rgba(0,0,0,0.5)",
+          },
+          container: {
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+            backgroundColor: "#fff",
+          },
+          draggableIcon: {
+            backgroundColor: "#000",
+          },
+        }}
+      >
+        <View className="">
+          <Image
+            className=""
+            style={{
+              width: "100%",
+              height: 200,
+              borderRadius: 10,
+              resizeMode: "contain",
+            }}
+            source={require("../assets/samosa.png")}
+          />
+          <View className="header pl-9  p-5 flex-1">
+            <Text className="text-xl text-left pt-5 font-bold text-red-500">
+              Tandoori Zinger{" "}
+            </Text>
+            <Text className="py-3 text-left text-lg font-semibold">₹200</Text>
+            <View className="flex-row">
+              <Text
+                className=" text-md-center text-gray-500 text-md"
+                numberOfLines={10}
+                style={{ flexWrap: "wrap", flex: 1 }}
+              >
+                Chicken zinger with a deliciout tandoori sauce
+              </Text>
+            </View>
+          </View>
+        </View>
+      </RBSheet>
+    </View>
+  );
+};
+const RenderItem = ({ item, key }) => {
+  return (
+    <>
+    <Item
+      key={key}
+      title={item.title}
+      price={item.price}
+      desc={item.desc}
+      img={item.img}
+    />
+    </>
+  );
+};
+const data = [
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+  {
+    title: "Tandoori Zinger",
+    price: 200,
+    desc: "Chicken zinger with a deliciout tandoori sauce",
+    img: require("../assets/samosa.png"),
+  },
+];
+const ItemList = () => {
+  const list = data.map((item, key) => (
+    <RenderItem key={key} item={item}></RenderItem>
+  ));
+  return <>{list}</>;
+};
+const Home = ({ navigation, title, width }) => {
+  return (
+    <Screen>
+      <View className="flex flex-col bg-gray-300 ">
+        <ScrollView stickyHeaderIndices={[0]} className=" min-h-fit">
+          <View className="flex py-3 px-5 justify-between flex-row bg-white " >
+            <View>
+              <Ionicons name="md-logo-firefox" size={34} color="#335566" />
+            </View>
+            <View className="flex flex-row">
+              <Ionicons name="md-search-sharp" size={34} color="#335566" />
+              <FontAwesome name="user-circle" size={34} color="#335566" />
+            </View>
+          </View>
+        <View className="px-3">
 
-        <TouchableOpacity onPress={()=>{
-          setFreeq(freq => freq+1);
-          setItems([...items,{item:'idely',value:1}])}} className=" self-center  mt-3">
-            <Text className="text-center px-8 py-2 rounded-xl text-white  " style={{fontSize:18}}><MaterialIcons name="add-circle" size={30} color="dodgerblue" /></Text>
-        </TouchableOpacity>
-       {freeq!==0 && <TouchableOpacity onPress={()=>{
-          items.pop();
-          setFreeq(freq =>freq-1)
-          setItems([...items])
-          }} className=" self-center  mt-3">
-            <Text className="text-center px-8 py-2 rounded-xl text-white" style={{fontSize:18}}><Ionicons name="ios-trash-bin" size={30} color="red" /></Text>
-        </TouchableOpacity>}
+          <ItemList />
         </View>
+          
         </ScrollView>
+        <View className="absolute w-full flex justify-center bottom-5">
+          <TouchableOpacity className="self-center py-2 px-5 bg-slate-700 rounded-full">
+            <Text className="self-center text-white text-md">VIEW MENU</Text>
+          </TouchableOpacity>
         </View>
-    </TailwindProvider>
-    </Screen>)
-}
-export default Home ;
+      </View>
+    </Screen>
+  );
+};
+export default Home;
